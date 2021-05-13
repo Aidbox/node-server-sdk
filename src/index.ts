e8b7cd17f9d68f5e2e60c4811fffaadf68a06152
Sergey Elsukov <oct8cat@gmail.com>
2021-05-13 09:53:04 +0300

    feat(manifest): Export readModulesManifest

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,4 @@ export * from './lib/app';
 export * from './lib/config';
 export { awaitAidbox } from './lib/agent';
 export * from './types';
-export { mergeManifests } from './lib/manifest';
+export { mergeManifests, readModulesManifests } from './lib/manifest';
