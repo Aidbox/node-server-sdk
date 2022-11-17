5e649c93e8ab9c740d4f964cb45cb9d78fda7fd1
Alexander Streltsov <funyloony@gmail.com>
2022-11-18 00:23:20 +0300

    feat: setup vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,26 @@
+export default {
+  title: 'Aidbox NodeJS SDK',
+  description: 'NodeJS SDK for build App on top of Aidbox',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          {text: 'Introduction', link: '/'},
+          {text: 'Getting Started', link: '/concept'},
+        ]
+      },
+      {
+        text: 'Usage',
+        items: [
+          {text: 'Manifest', link: '/usage/manifest'},
+          {text: 'Operation', link: '/usage/operation'},
+        ]
+      }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-present HealthSamurai'
+    }
+  },
+}
